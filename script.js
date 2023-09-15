6a61343c2c64292bdc23fd55f5d4f18396a9d246
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-15 12:14:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const btn = document.getElementById('incrementBtn')
+const c = document.getElementById('counter')
+btn.addEventListener('click',()=>{
+	alert(c.innerText)
+	c.innerText = Number(c.innerText)+1
+})
